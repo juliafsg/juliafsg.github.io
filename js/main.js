afcc976c84e9f9369129ff9fb8efb301b4371645
Júlia Ferreira <juliafsg@ufrn.edu.br>
2020-05-23 13:15:00 -0300

    feature/primeira versao

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    var i = 0;
+    var txt = 'Lorem ipsum dummy text blabla.';
+    var speed = 50;
+
+    function typeWriter() {
+        if (i < txt.length) {
+            document.getElementById("demo").innerHTML += txt.charAt(i);
+            i++;
+            setTimeout(typeWriter, speed);
+        }
+    }
+
+
+
+});
