17b334e2c97e271c0095c4d029e1b942cb62b998
Júlia Ferreira <juliafsg@ufrn.edu.br>
2020-05-26 20:26:28 -0300

    Animações, NavBar e Sobre mim

--- js/main.js
+++ js/main.js
@@ -1,15 +1,5 @@
 $(document).ready(function() {
-    var i = 0;
-    var txt = 'Lorem ipsum dummy text blabla.';
-    var speed = 50;
 
-    function typeWriter() {
-        if (i < txt.length) {
-            document.getElementById("demo").innerHTML += txt.charAt(i);
-            i++;
-            setTimeout(typeWriter, speed);
-        }
-    }
 
 
 
